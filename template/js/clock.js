98ebd0b93aafca0894d2696ad2d4b4e2ef51185a
durgeshsingh-17 <61835021+durgeshsingh-17@users.noreply.github.com>
2020-12-30 13:59:11 +0530

    Add files via upload

---
+++ template/js/clock.js
@@ -0,0 +1,11 @@
+function getTime(){
+	let today = new Date();
+
+	document.getElementById('date').innerHTML = today.toString().slice(0, 24);
+	setInterval("getTime()", 1000);
+}
+
+
+getTime();
+
+document.querySelector('#year').innerHTML = new Date().getFullYear();
