403fb31cf69771c186380f9e5edc46ffddfa94f1
durgeshsingh-17 <61835021+durgeshsingh-17@users.noreply.github.com>
2021-03-04 22:30:05 +0530

    Update clock.js

--- template/js/clock.js
+++ template/js/clock.js
@@ -1,11 +1,7 @@
 function getTime(){
 	let today = new Date();
-
 	document.getElementById('date').innerHTML = today.toString().slice(0, 24);
 	setInterval("getTime()", 1000);
 }
-
-
 getTime();
-
 document.querySelector('#year').innerHTML = new Date().getFullYear();
